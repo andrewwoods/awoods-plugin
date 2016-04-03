157726485aa5d14ce8c80b7a701001e0eb9344eb
Andrew Woods <atwoods1@gmail.com>
2016-04-03 08:57:52 -0700

    Remove hello world method

--- lib/wpcli/awoods.php
+++ lib/wpcli/awoods.php
@@ -4,36 +4,6 @@
  */
 class Awoods_Command extends WP_CLI_Command {
 
-	/**
-	 * Prints a greeting.
-	 *
-	 * ## OPTIONS
-	 *
-	 * <name>
-	 * : The name of the person to greet.
-	 *
-	 * [--type=<type>]
-	 * : Whether or not to greet the person with success or error.
-	 * ---
-	 * default: success
-	 * options:
-	 *   - success
-	 *   - error
-	 * ---
-	 *
-	 * ## EXAMPLES
-	 *
-	 *     wp awoods hello Newman
-	 *
-	 * @when before_wp_load
-	 */
-	function hello( $args, $assoc_args ) {
-		list( $name ) = $args;
-
-		// Print the message with type
-		WP_CLI::success( "Hello, $name!" );
-	}
-
 
 	/**
 	 * Prints a summary of the wordpress setup.
